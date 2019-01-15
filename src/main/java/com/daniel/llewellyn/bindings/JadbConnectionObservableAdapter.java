780deb0e6093ab7f65dd6dde4bfc3a5f835c5b36
Daniel Llewellyn <danieln.llewellyn@gmail.com>
2019-01-15 19:29:00 +0000

    Updating the observable

--- src/main/java/com/daniel/llewellyn/bindings/JadbConnectionObservableAdapter.java
+++ src/main/java/com/daniel/llewellyn/bindings/JadbConnectionObservableAdapter.java
@@ -1,6 +1,8 @@
 package com.daniel.llewellyn.bindings;
 
 import io.reactivex.Single;
+import io.reactivex.SingleEmitter;
+import io.reactivex.SingleOnSubscribe;
 import se.vidstige.jadb.JadbConnection;
 import se.vidstige.jadb.JadbDevice;
 
@@ -15,10 +17,23 @@ public class JadbConnectionObservableAdapter {
     }
 
     public Single<List<JadbDevice>> getDevices() {
-        return Single.create(emitter -> emitter.onSuccess(this.connection.getDevices()));
+        return Single.create(emitter -> {
+            try {
+                emitter.onSuccess(connection.getDevices());
+            } catch (Exception ex) {
+                emitter.onError(ex);
+            }
+        }
+        );
     }
 
     public Single<String> getHostVersion() {
-        return Single.create(emitter -> emitter.onSuccess(this.connection.getHostVersion()));
+        return Single.create(emitter -> {
+            try {
+                emitter.onSuccess(this.connection.getHostVersion());
+            } catch (Exception ex) {
+                emitter.onError(ex);
+            }
+        });
     }
 }
